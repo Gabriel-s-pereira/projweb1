a96ebe763079f2ed33b0a0ada46bdeb97ec3993d
Gabriel Pereira <112331277+Gabriel-s-pereira@users.noreply.github.com>
2026-06-19 11:37:07 -0300

    Add files via upload

--- projeto_luta/assets/js/model.js
+++ projeto_luta/assets/js/model.js
@@ -1,1 +1,22 @@
-console.log("hello");
+class Char{
+    constructor(nome, hp, sorte){
+        if (typeof(hp) == "number" && typeof(sorte) == "number"){ 
+            this.name = nome;
+            this.life = hp;
+            this.lucky = sorte;
+            }
+    }
+}
+
+const chaves = new Char("chaves",100,1);
+const girafales = new Char("Girafalles Vigarista",110,2);
+const madruga = new Char("Super Madrugadão",120,1);
+const quico = new Char("Quico",100,3);
+const chiquinha = new Char("Chiquinha",100,3);
+
+const lutador1 = document.querySelector(".sprit1");
+const lutador2 = document.querySelector(".sprit2");
+
+
+lutador1.innerHTML='<img src="assets/jpeg/girafalles.png" alt="professor girafales">';
+lutador2.innerHTML='<img src="assets/jpeg/madruga.png" alt="professor girafales">';
